c69347aadebb4467169697b273862c787c812fa9
Oluwadamilola Adebayo <88267825+DammyA7@users.noreply.github.com>
2022-09-30 14:22:54 +0100

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kButtonColor = Color(0xFF21D19F);
+const kBackgroundColor = Color(0xFFF6FEFC);
+const kloginSignupBackground = Color(0xFFFEFEFE);
+const kOnboardingTextStyle = TextStyle(color: Color(0xFF21D19F), fontWeight: FontWeight.w500,);
+const kactiveDotColor = Color(0xFF21D19F);
+const kActiveIconColor = Color(0xFF21D19F);
+const kInactiveDotColor = Color(0xFF727E7B);
+const kInactiveIconColor = Color(0xFF727E7B);
+const kGreyTextColor = Color(0xFF727E7B);
+const kGreenTextColor = Color(0xFF072C22);
+
+const kHomeIconPath = 'assets/icons/home.png';
+const kHistoryIconPath = 'assets/icons/history.png';
+const kNotificationsIconPath = 'assets/icons/notifications.png';
+const kSettingsIconPath = 'assets/icons/settings.png';
